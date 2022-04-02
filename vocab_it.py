d7dff6d1dd1c9e4b480386700c81c7e642c7396e
ml-utils <machinelearningutils@gmail.com>
2022-04-02 08:39:32 +0200

    added vocab data from blimp data generation project

--- vocab_it.py
+++ vocab_it.py
@@ -1,3 +1,115 @@
+human_nouns = ["amico", "collega", "compagno", "cliente", "ex", "donna", "uomo", "bambina", "bambino", "persona",
+               "cittadino", "individuo", "paziente", "persona", "personaggio", 'gente']
+nomen_agentis = ['lettore']
+animated_nouns_nonhuman_animals = ["animale", "cane", "gatto", "lupo", "leone", "tigre", "cavallo", "femmina",
+                                   "maschio", "pesce"]
+
 nouns_professions = ["dottore", "artista", "attore", "autore", "avvocato", "professore", "maestro", "scrittore",
                      "chimico", "corriere", "giornalista", "insegnante", "lavoratore", "maestro", "meccanico",
-                     "medico"]
+                     "medico", 'operaio']
+concrete_nouns_bodyparts = ["faccia", "bocca", "braccio", "gamba", "collo", "culo", "coglione", "corpo", "dente",
+                            "dito", "ala", "coda", "ginocchio", "indice", "labbro", "lingua", "cazzo", "mano",
+                            "muscolo", "naso", 'occhio', 'orecchio', 'pelle', 'piede']
+
+concrete_everyday_objects = ["foglio", "documento", "lettera", "anello", "corda", "bicchiere", "borsa", "bottiglia",
+                             "coltello", "chiave", "biglietto", "libro", "oggetto"]
+
+mass_uncountable_nouns = ["acqua", "caffè", "carta", "denaro", "soldi", "droga", "latte", "pane", "pasta"]
+
+
+intransitive_verbs_with_have_auxiliary = ["abitare", "agire", "ballare", "camminare", "dormire", "fare", "lavorare",
+                                          "parlare"]
+
+intransitive_verbs_with_be_auxiliary = ["andare", "bastare", "crescere", "entrare", "esistere", "essere", "stare"]
+
+'''' more words from fundamental vocabulary
+
+# nouns: 
+abito, aiuto,  ,  amore, aria, arrivo, azione, , bagno, battaglia, bellezza, calore, carne, colpa, commento, colore, desiderio, disegno, dubbio, inizio, parola
+acquisto, affare, centro,  appuntamento, arco, , arte, aspetto, assenza, associazione, attacco, atteggiamento, attenzione, avventura, azienda, banca, bar, base, cadavere, cambiamento, cambio, cultura, danno, domanda, dovere, energia, errore, età, favore, fuoco, gruppo, grasso, incontro, messaggio, mercato, mondo, musica
+
+# spatial nouns
+altezza, angolo, bordo, distanza, lato, apertura, buco, 
+# place nouns
+luogo, ambiente, casa, città, appartamento, laboratorio camera, edificio, entrata, fiume, lago, montagna, monte, 
+# time nouns
+gennaio..dicembre, estate, primavera, autunno, inverno, lunedì..domenica, giono, mese, anno, ora, minuto, secondo, momento, data, ieri, oggi, domani, sera, pomeriggio, notte, mattino
+
+# cardinal nouns
+uno..venti, cento, mille, miliardo, milione
+# quantitiy nouns
+metà, migliaio
+# perceptual nouns:
+ dolore, emozione, fame, fastidio, fatica, gusto, odore, sapore, luce, suono, rumore, voce, chiasso, 
+# abstract nouns
+libertà, idea, abitudine, accordo, accusa, bisogno, cosa, durata, esempio, minimo, massimo
+
+# more body-related nouns: 
+nudo, ferita, lacrima, sudore, malattia, salute, merda, piscio
+# concrete unanimated nouns: 
+albero, bosco, alba, aereo, albergo, auto, macchina, barca, sole, luna, terra, 
+# weather nouns:
+neve, pioggia, vento
+
+# furniture nouns: 
+letto, divano, finestra, mobile
+# clothin nouns: 
+maglia
+
+# nouns of materials: 
+legno
+
+# nouns for professions, actions, events: 
+caccia, colpo, comunicazione, corsa, bacio, calcio, discussione, incidente, lavoro, mestiere, morte, movimento
+
+# adjectives:  
+ facile, difficile, ancora, antico, assoluto, alcuno(i),  altro, , destro, , divertente, doppio, falso, vero, femminile, , giusto, importante, impossibile, incredibile, interessante, inutile, libero, liquido, solido, migliore, peggiore, uguale, identico, simile, diverso, minore, maggiore,  nuovo, vecchio, normale, 
+# spatial adjectives: 
+lontano, vicino, enorme, grande, ampio, largo, alto,  basso, piccolo, lungo, corto, interno, esterno, superiore, inferiore, centrale, laterale, posteriore, anteriore
+# temporal adjectives
+lento, veloce, breve, rapido
+# perceptual adjectives
+duro, molle, morbido, soffice, , caldo, freddo, fresco,, brutto, bello, leggero, pesante
+#taste adjectives buono, amaro, dolce, salato, aspro, 
+# color adjectives
+azzurro, bianco, blu, giallo, rosso, verde, nero, chiaro, scuro, buio
+# anited beings adjectives:
+malato, sano, buono, cattivo, felice, matto, pazzo, morto, vivo,
+# people adjectives:
+attento, bravo, intellligente, gentile, adulto, coglione
+
+# adjectives / past participles
+aperto, chiuso
+
+# verbs: 
+allontanare, allungare, alzare, ammettere, , appoggiare, arrestare, arrivare, ascoltare, aspettare, bruciare, cadere, cambiare, cantare, combattere, cominciare, commentare, comunicare, conoscere, controllare, correre, costruire, creare, credere, desiderare, dire, discutere, disegnare, distruggere, diventare, divertire, dimenticare, funzionare, ignorare, mancare, mandare, migliorare, morire, muovere, mentire, mettere, negare
+# transitive verbs: 
+accettare, accogliere, abbassare, accendere, accompagnare, accusare, aggiungere, aiutare, amare, ammazzare, attaccare, attraversare, aumentare, autorizzare, avanzare, avere, avvertire, avviare, avvicinare, cacciare, cancellare, chiamare, cucinare, cuocere, evitare, gettare, guardare, imparare, impedire, impegnare, incontrare, indicare,  insegnare, inserire, iniziare, minacciare, mostrare, nascondere
+# intransitive verbs, concrete actions: 
+aprire, battere, bere, baciare, buttare, bloccare, colpire, dare, fermare, lanciare, lasciare, lavare, legare, leggere, liberare, mangiare
+# intransitive/reflexive verbs: arrabbiarsi, accorgersi, ..bisogna, importare
+
+qua, quadro, qualche, qualcosa, qualcuno, quale, qualità, qualsiasi, qualunque, quando, quantità, quanto, quaranta, quartiere, quarto, quasi, quattro, quello, questione, questo, qui, quindi, quindici, quiz, quota, quotidiano
+rabbia, raccogliere, raccolta, raccontare, racconto, radice, radio, ragazza, ragazzino, ragazzo, raggio, raggiungere, ragione, rapido, rapporto, rappresentare, raro, re, reagire, reale, realizzare, realtà, reato, reazione, recare, recente, recitare, recuperare, regalare, regalo, reggere, regime, regina, regione, regista, registrare, registrazione, regno, regola, relativo, relazione, religione, religioso, rendere, repubblica, resistere, respirare, responsabile, responsabilità, restare, resto, rete, ricco, ricerca, ricetta, ricevere, richiamare, richiedere, richiesta, ricominciare, riconoscere, ricordare, ricordo, ridere, ridurre, riempire, rientrare, rifare, riferimento, riferire, rifiutare, rifiuto, riflessione, riflettere, riga, riguardare, riguardo, rilassare, rimanere, rimettere, ringraziare, rinunciare, ripetere, riportare, riprendere, riproduzione, risalire, rischiare, rischio, riservare, riservato, risorsa, rispettare, risolvere, risorsa, rispettare, rispetto, rispondere, risposta, ristorante, risultare, risultato, ritardo, ritenere, ritirare, ritmo, ritornare, ritorno, ritrovare, riunione, riunire, riuscire, rivedere, rivelare, rivista, rivolgere, rivoluzione, roba, romano, romanzo, rompere, rosa, rosso, rovinare, rubare, rumore, ruolo, russo, sabato, sacco, sala, salire, saltare, salutare, salute, saluto, salvare, salvo, sangue, sano, santo, sapere, sbagliare, sbagliato, sbattere, scala, scambiare, scambio, scappare, scaricare, scarpa, scatola, scattare, scegliere, scelta, scena, scendere, schermo, scherzare, schiena, schifo, scientifico, scienza, scomparire, sconosciuto, scoperta, scopo, scoppiare, scoprire, scorrere, scorso, scrittore, scrittura, scrivere, scuola, scusa, scusare, sé, se, secco, secolo, secondo, sede, sedere, sedia, segnalare, segnale, segnare, segno, segreto, seguente, seguire, seguito, sei, sembrare, semplice, semplicemente, sempre, seno, sensazione, senso, sentenza, sentimento, sentire, senza, separare, sera, serata, sereno, serie, serio, servire, servizio, sesso, sessuale, sette, settembre, settimana, settore, sezione, sfida, sforzo, sfruttare, sfuggire, sguardo, sì, si, sia, siccome, sicuramente, sicurezza, sicuro, sigaretta, significare, significato, signora, signore, signorina, silenzio, simbolo, simile, simpatico, singolo, sinistra, sinistro, sino, sistema, sistemare, sito, situazione, smettere, sociale, società, soddisfare, soddisfazione, sofferenza, soffrire, soggetto, soggiorno, sognare, sogno, solamente, soldato, soldo, sole, solito, sollevare, solo, soltanto, soluzione, somma, sondaggio, sonno, sopportare, sopra, soprattutto, sopravvivere, sorpresa, sorridere, sorriso, sorta, sospetto, sostanza, sostenere, sostituire, sottile, sotto, sottolineare, sottoporre, spagnolo, spalla, sparare, sparire, spaventare, spazio, specchio, speciale, specie, specifico, spegnere, spendere, speranza, sperare, spesa, spesso, spettacolo, spiaggia, spiegare, spiegazione, spingere, spirito, sporco, sport, sportivo, spostare, stabilire, staccare, stagione, stampa, stanco, stanza, stare, stasera, stato, stazione, stella, stesso, stile, stipendio, storia, storico, strada, straniero, strano, straordinario, strategia, strega, stretto, stringere, stronzo, strumento, struttura, studente, studiare, studio, stupido, su, subire, subito, succedere, successivo, successo, sud, sufficiente, suggerire, suo, suonare, suono, suora, superare, superficie, superiore, supporto, svegliare, sviluppare, sviluppo, svolgere, 
+tacere, tagliare, taglio, tale, talmente, tanto, tardi, tardo, tasca, tassa, tavola, tavolo, te, teatro, tecnica, tecnico, tecnologia, tedesco, telefonare, telefonata, telefonico, telefono, televisione, televisivo, tema, temere, temperatura, tempo, tenda, tendenza, tendere, tenere, tensione, tentare, tentativo, teoria, terapia, terminare, termine, terra, terreno, terribile, territorio, terzo, tesi, tesoro, test, testa, testimone, testo, tetto, ti, tipico, tipo, tirare, titolo, tivu, toccare, togliere, tono, tornare, totale, tra, traccia, tradizionale, tradizione, tradurre, traduzione, traffico, tramite, tranne, tranquillo, trarre, trascinare, trascorrere, trasferire, trasformare, trasmettere, trasmissione, trasporto, trattamento, trattare, trattenere, tratto, tre, treno, trenta, tribunale, triste, troppo, trovare, tu, tuo, turno, tuttavia, tutto, 
+uccello, uccidere, ufficiale, ufficio, uguale, ulteriore, ultimo, umanità, ulteriore, umano, una, unico, unire, unità, unito, università, universo, uno, uomo, uovo, urlare, usare, uscire, uscita, uso, utente, utile, utilizzare, 
+vacanza, valere, valore, valutare, vantaggio, vario, ve, vecchio, vedere, veloce, velocità, vendere, vendita, venerdì, venire, venti, vento, veramente, verbale, verde, verificare, verità, vero, versare, versione, verso, vestire, vestito, vetro, vi, via, viaggiare, viaggio, vicenda, vicino, video, villa, villaggio, vincere, vino, violento, violenza, visione, visita, visitare, viso, vista, vita, vittima, vittoria, vivere, vivo, voce, voglia, voi, volare, volere, volo, volontà, volta, voltare, volto, volume, vostro, votare, voto, vuoto
+zero, zitto, zona
+
+o, obbligare, obiettivo, occasione, occhiale, , occidentale, occorrere, occhiale, occupare, odiare, , offerta, offrire, , oggi, ogni, ognuno, ok, okay, olio, oltre, ombra, omicidio, onda, onore, opera, , operare, operazione, opinione, opporre, opposto, oppure, ora, ora, orario, ordinare, ordine, , organismo, organizzare, organizzazione, organo, originale, origine, ormai, oro, orologio, ospedale, ospitare, ospite, osservare, osso, ottenere, ottimo, otto, ottobre, ovunque, ovvero, ovviamente, 
+pace, padrone, paese, pagamento, pagare, pagina, paio, palazzo, palla, , pantalone, parco, parecchio, parere, parete, pari, , , parte, partecipare, partenza, particolare, particolarmente, partire, partita, partito, passaggio, passare, passato, passione, passo, , paura, pausa, , pazienza, pazzo, peggio, , pena, pensare, pensiero, pensione, per, percepire, perché, perciò, percorso, perdere, perdita, perdonare, perfettamente, perfetto, perfino, pericolo, pericoloso, periodo, permettere, però, persino, , personale, personalmente, pesante, , peso, petto, piacere, pezzo, pianeta, piangere, piano, pianta, piatto, piazza, piccolo, , pieno, pietra, pioggia, pistola, piu, piuttosto, poco, poesia, poeta, poi, poiché, politica, politico, polizia, poliziotto, polvere, pomeriggio, ponte, popolare, popolazione, popolo, porre, porta, portare, porto, positivo, posizione, possedere, possibile, possibilità, posta, posto, potente, potere, potere, povero, pranzo, pratica, praticamente, pratico, precedente, precisare, preciso, preferire, pregare, prego, premio, prendere, preoccupare, preparare, presa, presentare, presente, presenza, presidente, pressione, presso, prestare, presto, prete, pretendere, prevedere, previsto, prezioso, prezzo, prima, primavera, principale, principe, principio, privato, probabilmente, problema, procedere, procedura, processo, prodotto, produrre, produzione, professionale, professore, profilo, profondo, progetto, programma, promettere, pronto, pronunciare, proporre, proposito, proposta, proprietà, proprietario, proprio, proseguire, prossimo, protagonista, proteggere, prova, provare, provenire, provincia, provocare, pubblicare, pubblicità, pubblico, pugno, pulire, punta, puntare, punto, puro, purtroppo, 
+
+, , , , , , , , , , , , campo, , , , canto, canzone, capace, capacità, capello, capire, capitare, capo, carattere, caratteristica, carica, caricare, carico, carino, , caro, , , casino, caso, cassa, castello, categoria, catena, , causa, causare, cavaliere, , , cedere, cellula, cellulare, cena, centinaio, centrale,cercare, certamente, certezza, certo, cervello, , chiaramente, chiarire, , , chiedere, chilometro, , chissà, chiudere, , , ci, ciao, , cibo, cielo, cifra, cinema, cinese, cinquanta, cinque, ciò, cioè, circa, circondare, circostanza, citare, , , civile, classe, classico, ,codice, cogliere, , coinvolgere, collegare, , colonna, , , commettere, commissione, comodo, compagnia, , comparire, compiere, compito, complesso, completamente, completo, comporre, comportamento, comportare, compreso, computer, comune, , , comunista, comunità, , , concentrare, concerto, concreto, , condividere, condizione, condurre, confermare, confine, confondere, confronto, 
+concludere, conclusione, condannare, 
+conoscenza, conquistare, consegnare, conseguenza, consentire, conservare, considerare, considerazione, consigliare, consiglio, consumare, contare, contatto, contemporaneo, contenere, contento, contenuto, contesto, continuare, continuo, conto, contrario, contratto, , controllo, conversazione, convincere, copia, coppia, coprire, coraggio, , , correggere, corrente, , , corrispondere, , corso, corte, , , coscienza, , cosiddetto, costa, costare, costituire, costo, costringere, , costruzione, costume,  credito, 
+
+, crescita, crisi, critica, critico, cucina, , cui, , culturale, cuore, cura, curare, curioso, , , , , dato, davvero, debito, debole, decidere, decina, decisione, dedicare, definitivo, definizione, democrazia, denunciare, derivare, descrivere, descrizione, destinare, destino, determinare, determinato, dettaglio, dialetto, dialogo, diamante, diavolo, dichiarare, dichiarazione,  , difendere, difesa, differente, differenza, , difficoltà, diffondere, dimensione, , dimostrare, dio, dipendente, dipendere, , direttamente, diretto, direttore, direzione, dirigere, diritto, disco, discorso, , , , dispiacere, disponibile, disporre, disposizione, , distinguere, distribuire, , , divenire, , , , dividere, , , , dollaro, , , , domandare, , , don, , , , , , , ,, , , , , durare, , , , , economia, economico, edicola, , editore, edizione, educazione, , effetto, effettuare, egli, elegante, elementare, elemento, elenco, elettrico, elettronico, eliminare, e-mail, emergere, , , , ente, , , , , episodio, epoca, eppure, equilibrio, era, eroe, , esame, esattamente, esatto, escludere, eseguire, , esercizio, esigenza, esistenza, , esperienza, esperto, esporre, espressione, esprimere, essa, , essi, esso, , , estero, estremo, , euro, europeo, evento, eventuale, evidente, evidentemente, , 
+, fabbrica, , , facilmente, , , famoso, fantasia, fantastico, , fascia, fase, , , fatto, fattore, , fede, , , , fenomeno, , , fermo, ferro, festa, festeggiare, fianco, fidarsi, fiducia, figura, figurare, fila, file, film, filo, filosofia, finale, finalmente, finanziario, , fine, , fingere, finire, fino, finto, fiore, firma, firmare, fisico, fissare, fisso, fondamentale, fondare, fondo, fonte, forma, formare, formazione, formula, fornire, , forte, fortuna, forza, foto, fotografia, , francese, frase, fratello, , fregare, frequentare, , fregare, fretta, fronte, frutto, fuga, fuggire, fumare, fumo, , funzione, , fuori, futuro, 
+, gara, garantire, , generale, generare, generazione, genere, , , gestione, gestire, gesto, , già, giacca, , giapponese, giardino, , giocare, giocatore, gioco, gioia, giornale, , giornata, giorno, giovane, girare, giro, giu', giudicare, giudice, giudizio, giugno, giungere, giurare,
+ giustizia, , gli, godere, governo, grado, grave, grazie, greco, gridare, grigio, grosso, , guadagnare, , guardia, guerra, giuda, guidare, , , ideale, identificare, identità, idiota, , , il, immaginare, immagine, , immobile,  impegno, impianto, impiegare, imporre, , importanza, , , impresa, impressione, improvviso, imputato, , , , , , indagine, , indicazione, , , indirizzo, individuare, , indossare, industria, industriale, infatti, inferiore, infilare, infine, infinito, informare, informazione, inglese, ingresso, iniziale, , iniziativa, , innamorarsi, inoltre, ,insieme, insistere, insomma, intanto, , intendere, intenso, intenzione, , interessare, interesse, internazionale, internet, , intero, interpretare, interrompere, intervenire, intervento, intervista, intorno, introdurre, , invece, inventare, inverno, investire, inviare, invitare, io, ipotesi, iscrivere, isola, ispirare, istante, istituto, istituzione, istruzione, italiano, , la, , , , , lamentare, , , , , , , , , , le, le, legale, , legato, legge, , , , lei, , , , letteratura, , , lettura, levare, lezione, lì, li, , , , , limitare, limite, linea, , linguaggio, , lira, lista, litigare, livello, lo, locale, , loro, lotta, , , , , .
+macchia, , , magari, maggioranza, magico, , , , , ,  , maniera, manifestare, manifestazione, , mantenere, marco, mare, maschile, maschio, massa, massimo, materia, materiale, mattina, , , meccanismo, medicina, media, , , membro, memoria,  mentale, mentre, mente, , meraviglioseo, , ,
+ meridionale, meritare, merito, mese, , , , metodo, metro, , mezzo, mezzo, mi, mica, , , , mila, , , , ministero, ministro, , minuto, mio, miracolo, missione, mistero, misura, , moda, modello, moderno, modificare, modo, , , mondiale, , moneta, morale, , , , mostra, , motivo, moto, motore, , , muro, , museo, , musicale
+ , nascosto, , nastro, natura, naturale, naturalmente, nave, nazione, né, ne, neanche, nemmeno, neppure, necessario, necessità, , negativo, negozio, nemico, nervoso, no, nome, nominare, non, nonostante, nord, sud, ovest, est, normale, nota, notare, notizia, , noto, novità, nucleare, , , numero, numeroso,  
+
+ world knowledge: capitale, camion, capitolo, carriera
+'''
