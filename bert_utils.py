- changin imports of pytorch_pretrained_bert to transformers - fixing tqdm total count in unit tests - using almostEqual in unit tests, with 3 digits of precision

bert_utils.py
@@ -2,10 +2,9 @@ import math
 
 import torch
 from torch.nn.functional import softmax
-# from pytorch_pretrained_bert import BertForMaskedLM, tokenization
-# from pytorch_pretrained_bert.tokenization import BertTokenizer
-from pytorch_pretrained_bert.modeling import BertPreTrainedModel
-from transformers import BertTokenizer, BertForMaskedLM
+from transformers import BertTokenizer, BertForMaskedLM as BertPreTrainedModel
+
+from transformers.modeling_outputs import MaskedLMOutput
 
 import numpy as np
 
@@ -316,7 +315,8 @@ def get_bert_output(bert: BertPreTrainedModel, tokenizer: BertTokenizer, sentenc
     tens = torch.LongTensor(sentence_ids).unsqueeze(0)
 
     res_unsliced = bert(tens)
-
+    if isinstance(res_unsliced, MaskedLMOutput):
+        res_unsliced = res_unsliced.logits
     # print(f'masked_word_idx: {masked_word_idx}, type(res_unsliced): {type(res_unsliced)}')
     # masked_word_idx: 1, type(res_unsliced): <class 'transformers.modeling_outputs.MaskedLMOutput'>
     # masked_word_idx: 5, type(res_unsliced): <class 'torch.Tensor'>

compare_compute_model_score.py
@@ -2,10 +2,7 @@ import torch
 from transformers import GPT2Tokenizer, GPT2LMHeadModel  # pytorch_transformers
 
 from transformers import BertTokenizer, BertForMaskedLM
-# from pytorch_pretrained_bert.tokenization import BertTokenizer
-# from pytorch_pretrained_bert.modeling import BertPreTrainedModel
 from transformers import BertForMaskedLM
-# from pytorch_pretrained_bert import BertForMaskedLM
 
 from scipy.special import softmax
 import numpy as np
@@ -29,7 +26,7 @@ def run_testset(model_type, model_name, device, testset):
         model = GPT2LMHeadModel.from_pretrained(model_name)
         tokenizer = GPT2Tokenizer.from_pretrained(model_name)
     elif model_type == model_types.BERT:
-        model = BertPreTrainedModel.from_pretrained(model_name)  # BertForMaskedLM.from_pretrained(model_name)
+        model = BertForMaskedLM.from_pretrained(model_name)  # BertForMaskedLM.from_pretrained(model_name)
         tokenizer = BertTokenizer.from_pretrained(model_name,
                                               do_lower_case=(True if "uncased" in model_name else False))
     else: