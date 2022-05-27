a8cd7a887c2a863f2d606ac74769eb14ae2e2137
ml-utils <machinelearningutils@gmail.com>
2022-05-27 14:13:47 +0200

    - auto code formatting from black pre commit hook

--- run_tests.py
+++ run_tests.py
@@ -1,15 +1,18 @@
-import vocab_it
+from vocabulary import vocab_it
 
 
 def t_determiner_noun_agreement_1():
     from generation_projects.blimp import determiner_noun_agreement_1
+
     generator = determiner_noun_agreement_1.DetNGenerator()
-    generator.generate_paradigm(rel_output_path="outputs/blimp/%s.jsonl" % generator.uid)
+    generator.generate_paradigm(
+        rel_output_path="outputs/blimp/%s.jsonl" % generator.uid
+    )
 
 
 def print_profession_nouns():
     for noun in vocab_it.nouns_professions:
-        print(noun + ' ')
+        print(noun + " ")
 
 
 def main():
