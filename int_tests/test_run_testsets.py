e5a8b31f433098ffa1ca45d2cc62bfb0ae510b5d
ml-utils <machinelearningutils@gmail.com>
2022-06-23 08:34:01 +0200

    refactoring: reordering int. tests

--- int_tests/test_run_testsets.py
+++ int_tests/test_run_testsets.py
@@ -110,60 +110,6 @@ class TestRunTestSets(TestCase):
         )
 
     @staticmethod
-    def profile_slowdowns():
-        import cProfile
-        import pstats
-        import os
-        import tqdm
-        import json
-
-        model_type = ModelTypes.ROBERTA  # ModelTypes.GPT  #
-        model_name = "roberta-large"  # "roberta-base" #"gpt2-medium"
-        # "gpt2-large"  # 'gpt2' #  "bert-large-uncased"
-        model, tokenizer = load_model(model_type, model_name, DEVICES.CPU)
-
-        p = get_test_data_dir() / "blimp"
-        testset_dir_path = str(p)
-
-        testset_filename = "mini_wh_island.jsonl"
-        testset_filepath = os.path.join(testset_dir_path, testset_filename)
-
-        print(f"loading testset file {testset_filepath}..")
-        with open(testset_filepath, "r") as json_file:
-            json_list = list(json_file)
-        print("testset loaded.")
-
-        examples = []
-        for json_str in tqdm(json_list):
-            example = json.loads(json_str)
-
-            sentence_good = example[SentenceNames.SENTENCE_GOOD]
-            sentence_bad = example[SentenceNames.SENTENCE_BAD]
-            examples.append(
-                {
-                    SentenceNames.SENTENCE_GOOD: sentence_good,
-                    SentenceNames.SENTENCE_BAD: sentence_bad,
-                    SentenceNames.SENTENCE_GOOD_2ND: "",
-                }
-            )
-        testset = {"sentences": examples}
-        sentences_per_example = 2
-
-        with cProfile.Profile() as pr:
-            run_testset(
-                model_type,
-                model,
-                tokenizer,
-                DEVICES.CPU,
-                testset,
-                sentences_per_example,
-            )
-
-        stats = pstats.Stats(pr)
-        stats.sort_stats(pstats.SortKey.TIME)
-        stats.print_stats()
-
-    @staticmethod
     def run_syntactic_tests_it_legacy_impl(
         model_type, testset_filenames=None, testset_dir_path=None
     ):
@@ -226,3 +172,57 @@ class TestRunTestSets(TestCase):
                     testset_data,
                     sentences_per_example,
                 )
+
+    @staticmethod
+    def profile_slowdowns():
+        import cProfile
+        import pstats
+        import os
+        import tqdm
+        import json
+
+        model_type = ModelTypes.ROBERTA  # ModelTypes.GPT  #
+        model_name = "roberta-large"  # "roberta-base" #"gpt2-medium"
+        # "gpt2-large"  # 'gpt2' #  "bert-large-uncased"
+        model, tokenizer = load_model(model_type, model_name, DEVICES.CPU)
+
+        p = get_test_data_dir() / "blimp"
+        testset_dir_path = str(p)
+
+        testset_filename = "mini_wh_island.jsonl"
+        testset_filepath = os.path.join(testset_dir_path, testset_filename)
+
+        print(f"loading testset file {testset_filepath}..")
+        with open(testset_filepath, "r") as json_file:
+            json_list = list(json_file)
+        print("testset loaded.")
+
+        examples = []
+        for json_str in tqdm(json_list):
+            example = json.loads(json_str)
+
+            sentence_good = example[SentenceNames.SENTENCE_GOOD]
+            sentence_bad = example[SentenceNames.SENTENCE_BAD]
+            examples.append(
+                {
+                    SentenceNames.SENTENCE_GOOD: sentence_good,
+                    SentenceNames.SENTENCE_BAD: sentence_bad,
+                    SentenceNames.SENTENCE_GOOD_2ND: "",
+                }
+            )
+        testset = {"sentences": examples}
+        sentences_per_example = 2
+
+        with cProfile.Profile() as pr:
+            run_testset(
+                model_type,
+                model,
+                tokenizer,
+                DEVICES.CPU,
+                testset,
+                sentences_per_example,
+            )
+
+        stats = pstats.Stats(pr)
+        stats.sort_stats(pstats.SortKey.TIME)
+        stats.print_stats()
