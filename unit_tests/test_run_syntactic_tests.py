ced9e558ddbf3c7cb3511324fa97e030652c02f7
ml-utils <machinelearningutils@gmail.com>
2022-06-27 15:30:10 +0200

    fixed wrong imports that were crashing tox after project restructuring (2); readded py39 to tox

--- unit_tests/test_run_syntactic_tests.py
+++ unit_tests/test_run_syntactic_tests.py
@@ -2,7 +2,6 @@ from unittest import TestCase
 
 import pytest
 
-from src.linguistic_tests.run_legacy_tests import run_tests_for_model_type
 from src.linguistic_tests.run_test_design import (
     rescore_testsets_and_save_pickles,
 )
@@ -12,9 +11,4 @@ class TestRunSyntacticTests(TestCase):
     @pytest.mark.skip("todo")
     def test_rescore_testsets_and_save_pickles(self):
         rescore_testsets_and_save_pickles()
-        raise NotImplementedError
-
-    @pytest.mark.skip("todo")
-    def test_run_tests_for_model_type(self):
-        run_tests_for_model_type()
         raise NotImplementedError
