ca55dbe79a319d31ca1b91789cbea55e1e024771
ml-utils <machinelearningutils@gmail.com>
2022-07-02 18:17:56 +0200

    interactive mode: (WIP) adding scoring of a 4 sentence item

--- src/linguistic_tests/notebook.py
+++ src/linguistic_tests/notebook.py
@@ -3,9 +3,10 @@ import sys
 
 from src.linguistic_tests.bert_utils import analize_example
 from src.linguistic_tests.lm_utils import DEVICES
-from src.linguistic_tests.lm_utils import get_models_dir
 from src.linguistic_tests.lm_utils import get_num_of_available_cuda_gpus
 from src.linguistic_tests.lm_utils import load_model_and_tokenizer
+from src.linguistic_tests.lm_utils import MODEL_TYPES_AND_NAMES_EN
+from src.linguistic_tests.lm_utils import MODEL_TYPES_AND_NAMES_IT
 from src.linguistic_tests.lm_utils import ModelTypes
 from src.linguistic_tests.lm_utils import print_red
 from src.linguistic_tests.lm_utils import red_txt
@@ -14,86 +15,144 @@ from src.linguistic_tests.plots_and_prints import print_detailed_sentence_info
 from src.linguistic_tests.run_test_design import run_test_design
 
 
-def interactive_mode():
+def get_interactive_mode_arg_parser():
+    import argparse
+
+    arg_parser = argparse.ArgumentParser()
+
+    # example: --cmd example --s_ni ".." --l_ni ".." --s_is ".." --l_is ".."
+    arg_parser.add_argument("--cmd", type=str, choices=["compare2", "example", "exit"])
+    arg_parser.add_argument("--s_ni", type=str)
+    arg_parser.add_argument("--l_ni", type=str)
+    arg_parser.add_argument("--s_is", type=str)
+    arg_parser.add_argument("--l_is", type=str)
+
+    return arg_parser
+
+
+def interactive_mode(device=DEVICES.CPU):
     print("interactive mode")
+
+    import shlex
+
+    # todo: interactive mode in which a whole command (pair of sentences) is passed in a single line
+    # (each time keeping the session line for the next command, because there is a script startup delay
+    # params: type of comparison/checks (..), s1_txt, s2_txt (within quotation marks)
+    # model (can pass a list of models to load at startup)
+    #
+    # options for type of comparison/checks:
+    # print ..
+    # plot ..
 
     # todo: test tokenization with sentencepiece, check no unknown
     # todo: check topk
     # todo: list special tokens
 
     # load model than wait for input sentences
-    scorebase = sentence_score_bases.LOGISTIC_FUN
-    print(f"Scores are based on scorebase={scorebase}")
-    model_dir = str(
-        # get_models_dir() / "bostromkaj/bpe_20k_ep20_pytorch"
-        get_models_dir()
-        / "bostromkaj/uni_20k_ep20_pytorch"
-        # "bert-base-uncased",
-        # str(get_models_dir() / "bert-base-italian-xxl-cased")
-    )
-    model_name = model_dir
-    model_type = ModelTypes.ROBERTA  # ModelTypes.BERT  #
+    scorebase = sentence_score_bases.SOFTMAX  # LOGISTIC_FUN
+    print(f"Scores are based on scorebase={scorebase.name}")
+
+    model_types_and_names = {**MODEL_TYPES_AND_NAMES_EN, **MODEL_TYPES_AND_NAMES_IT}
+
+    # local_model_names = [
+    #     str(get_models_dir() / "bostromkaj/bpe_20k_ep20_pytorch"),
+    #     str(get_models_dir() / "bostromkaj/uni_20k_ep20_pytorch"),
+    # ]
+    model_names = list(model_types_and_names.keys())
+    model_choice_correct = False
+    while not model_choice_correct:
+        print("These are the models available, pick a choice:")
+        for idx, model_name in enumerate(model_names):
+            print(f"({idx}) {model_name}")
+
+        model_choice = input("Enter a model number: ")
+        try:
+            model_name = model_names[int(model_choice)]
+            model_choice_correct = True
+        except (IndexError, ValueError) as err:
+            print(f"{err}, {type(err)}")
+
+    model_type = model_types_and_names[
+        model_name
+    ]  # ModelTypes.ROBERTA  # ModelTypes.BERT  #
     # eval_suite = 'it'
     model, tokenizer = load_model_and_tokenizer(
-        model_type, model_name, DEVICES.CPU, do_lower_case=False
+        model_type, model_name, device, do_lower_case=False
     )
 
-    print("model loaded, waiting for sentences..")
+    print("model loaded, waiting for commands..")
+    parser = get_interactive_mode_arg_parser()
 
     # given two sentences, print PenLPs, and diff btw PenLPs
     end_program = False
     while not end_program:
-        good_sentence = input("Enter first sentence (good): ")
-        if good_sentence == "exit":
+        argString = input("Enter command: ")
+        args = parser.parse_args(shlex.split(argString))
+
+        if args.cmd == "exit":
             return
-        bad_sentence = input("Enter 2nd sentence (bad): ")
+        elif args.cmd == "compare2":
+            compare_two_sentences(args, model, tokenizer)
+        elif args.cmd == "example":
+            score_example(args, model, tokenizer)
 
-        for sentence in [good_sentence, bad_sentence]:
-            tokens = tokenizer.tokenize(sentence)
-            print(
-                f"{sentence} \n "
-                f"tokenized as: {tokens} \n "
-                f"with ids: {tokenizer.convert_tokens_to_ids(tokens)}"
-            )
 
-        # todo: print unk token and id
+def score_example(args, model, tokenizer):
+    raise NotImplementedError
 
-        example = {
-            "good_sentence": good_sentence,
-            "bad_sentence": bad_sentence,
-            "good_sentence2": None,
-        }
-        sentences_per_example = 2
-        (
-            base_sentence_less_acceptable,
-            second_sentence_less_acceptable,
-            acceptability_diff_base_sentence,
-            acceptability_diff_second_sentence,
-            penLP_base_sentence,
-            penLP_bad_sentence,
-            penLP_2nd_good_sentence,
-            # logits_normalized_bad_sentence,
-            # logits_normalized_base_sentence,
-            # logits_normalized_2nd_good_sentence,
-            oov_counts,
-        ) = analize_example(
-            model, tokenizer, -1, example, sentences_per_example, score_based_on=None
-        )
 
-        diff_penLP = round(penLP_base_sentence - penLP_bad_sentence, 3)
+def compare_two_sentences(args, model, tokenizer):
+    # good_sentence = input("Enter first sentence (good): ")
+    # bad_sentence = input("Enter 2nd sentence (bad): ")
+    good_sentence = args.good
+    bad_sentence = args.bad
 
-        print_red("PenLP:")
+    for sentence in [good_sentence, bad_sentence]:
+        tokens = tokenizer.tokenize(sentence)
         print(
-            f"Diff {red_txt(diff_penLP)}, "
-            f"good ({penLP_base_sentence:.1f}), "
-            f"bad ({penLP_bad_sentence:.1f}): "
-            f"{good_sentence} || {bad_sentence}"
+            f"{sentence} \n "
+            f"tokenized as: {tokens} \n "
+            f"with ids: {tokenizer.convert_tokens_to_ids(tokens)}"
         )
 
-        # analize both sentences with topk for each masking
-        if diff_penLP >= 0:
-            print_detailed_sentence_info(model, tokenizer, good_sentence, scorebase)
-            print_detailed_sentence_info(model, tokenizer, bad_sentence, scorebase)
+    # todo: print unk token and id
+
+    example = {
+        "good_sentence": good_sentence,
+        "bad_sentence": bad_sentence,
+        "good_sentence2": None,
+    }
+    sentences_per_example = 2
+    (
+        base_sentence_less_acceptable,
+        second_sentence_less_acceptable,
+        acceptability_diff_base_sentence,
+        acceptability_diff_second_sentence,
+        penLP_base_sentence,
+        penLP_bad_sentence,
+        penLP_2nd_good_sentence,
+        # logits_normalized_bad_sentence,
+        # logits_normalized_base_sentence,
+        # logits_normalized_2nd_good_sentence,
+        oov_counts,
+    ) = analize_example(
+        model, tokenizer, -1, example, sentences_per_example, score_based_on=None
+    )
+
+    diff_penLP = round(penLP_base_sentence - penLP_bad_sentence, 3)
+
+    print_red("PenLP:")
+    print(
+        f"Diff {red_txt(diff_penLP)}, "
+        f"good ({penLP_base_sentence:.1f}), "
+        f"bad ({penLP_bad_sentence:.1f}): "
+        f"{good_sentence} || {bad_sentence}"
+    )
+
+    # analize both sentences with topk for each masking
+    if diff_penLP >= 0:
+        print_detailed_sentence_info(model, tokenizer, good_sentence, scorebase=None)
+        print_detailed_sentence_info(model, tokenizer, bad_sentence, scorebase=None)
 
 
 # todo same gpt2 as in the paper, comparable bert
@@ -147,7 +206,7 @@ def main(
     print_red(f"Using cuda device {device}")  # logging.info
 
     if len(sys.argv) > 1:
-        interactive_mode()
+        interactive_mode(device)
     else:
         # todo: save accuracy and other results to csv file (for import in excel table)
         #  also another csv file with details on sentences scores
